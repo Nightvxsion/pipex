593310fd9a5a39f72011537718090eabcc434cac
marcgar2 <marcgar2@student.42madrid.com>
2025-01-31 23:53:21 +0100

    STARTING PIPEX

---
+++ inc/pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcgar2 <marcgar2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 23:41:10 by marcgar2          #+#    #+#             */
+/*   Updated: 2025/01/31 23:41:10 by marcgar2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+#endif
