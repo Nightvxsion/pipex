69248282f0f54b05edb84cfc59614f9d5a0c929a
marcgar2 <marcgar2@student.42madrid.com>
2025-02-02 23:35:59 +0000

    MANDATORY DONE WTF

--- inc/pipex.h
+++ inc/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nightvision <nightvision@student.42.fr>    +#+  +:+       +#+        */
+/*   By: marcgar2 <marcgar2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 23:41:10 by marcgar2          #+#    #+#             */
-/*   Updated: 2025/02/01 22:36:13 by nightvision      ###   ########.fr       */
+/*   Updated: 2025/02/02 23:21:26 by marcgar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,12 @@
 char	*look_for_path(char *cmd, char **envp);
 void	disp_error(void);
 void	exec(char *argv, char **envp);
+
+//PIPEX
+void	childs(char **argv, char **envp, int *fd);
+void	parents(char **argv, char **envp, int *fd);
+
+//UTILS BONUS
+void	use(void);
+int		open_file_mode(char *argv, int i);
 #endif
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -12,3 +12,28 @@
 
 #include "../inc/pipex.h"
 
+void	childs(char **argv, char **envp, int *fd)
+{
+	int input;
+
+	input = open(argv[1], O_RDONLY, 0777); //0777 es un permiso general en el que todo vale
+	if (input == -1)
+		disp_error();
+	dup2(fd[1], STDOUT_FILENO); //Recibe los datos de input (STDIN)
+	dup2(input, STDIN_FILENO); //Lo que haya en input se lo pasa al fd 1 (STDOUT)
+	close(fd[0]);
+	exec(argv[2], envp); //Ejecuta el argumento 2 de parametro y lo compara con las ENVP
+}
+
+void	parents(char **argv, char **envp, int *fd)
+{
+	int	output;
+
+	output = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (output == -1)
+		disp_error();
+	dup2(fd[0], STDOUT_FILENO);
+	dup2(output, STDIN_FILENO);
+	close(fd[1]);
+	exec(argv[3], envp);
+}
