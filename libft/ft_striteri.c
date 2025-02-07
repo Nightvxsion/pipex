c9935b638d6bcb985c298f48c9dd00d7e5173763
NightVision <nightvisionmorespace@gmail.com>
2025-02-07 10:08:04 +0000

    ALMOSTR DONE MAKEFILE

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -3,20 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcgar2 <marcgar2@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: marcgar2 <marcgar2@student.42madrid.org    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 19:16:13 by marcgar2          #+#    #+#             */
-/*   Updated: 2024/10/08 19:17:18 by marcgar2         ###   ########.fr       */
+/*   Updated: 2025/02/07 10:06:18 by marcgar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void	cambiar_a_mayuscula_si_impar(unsigned int i, char *c)
-{
-	if (i % 2 == 0 && *c >= 'a' && *c <= 'z')
-		*c = *c - 32;
-}
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
